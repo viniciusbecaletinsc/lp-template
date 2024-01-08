17216e897a91a0700266dc060b81e3797511bf1d
Vinicius Becaleti <vinicius.becaleti@gruponsc.com.br>
2024-01-08 01:22:41 -0300

    refact: refatoração do navbar

--- src/components/ui/navbar/Navbar.vue
+++ src/components/ui/navbar/Navbar.vue
@@ -4,23 +4,19 @@ import { cn } from '@/lib/utils'
 const props = defineProps({
   class: {
     type: String,
-    default: '',
-  },
+    default: ''
+  }
 })
 </script>
 
 <template>
   <div
-    :class="
-      cn('py-4 grid grid-cols-[1fr_auto] items-center justify-between gap-4',
-        props.class,
-      )
-    "
+    :class="cn('relative flex h-16 items-center bg-primary text-primary-foreground', props.class)"
   >
-    <slot />
+    <div :class="cn('container flex items-center justify-between')">
+      <slot />
+    </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
